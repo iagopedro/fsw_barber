59a0bfed390c157221db9a4709a7bc3a9d925b75
iagopedro <iagopedroltd19@gmail.com>
2024-02-11 09:20:43 -0300

    feat: add bookings page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -28,7 +28,7 @@ export default async function Home() {
 
       <div className="px-5 mt-6">
         <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">Agendamentos</h2>
-        <BookingItem />
+        {/* <BookingItem /> */}
       </div>
 
       <div className="mt-6">
